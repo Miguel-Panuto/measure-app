2fe66932fdb3952cf0ca9dd226e002f15d1e75f3
Miguel Panuto <miguel.panuto@live.com>
2020-05-11 17:30:52 -0300

    no lopping cores

--- pc-side/src/index.ts
+++ pc-side/src/index.ts
@@ -1,29 +1,24 @@
-import readline from 'readline';
+import os from 'os';
+import fs from 'fs';
+
 import api from './services/api';
 
-import cpuUsage from './cpu-usage';
-import ramUsage from './ram-usage';
-import strUsage from './str-usage';
+import cpuUsage from './usages/cpu-usage';
+import ramUsage from './usages/ram-usage';
+import strUsage from './usages/str-usage';
 
-
-
-const read = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-read.question('Your name?', name => {
-    api.post('/user', {
-        name
-    }).then(res => {
-        const id = res.data.id;
-        read.close();
-        cpuUsage(id);
-        ramUsage(id);
-        strUsage(id);
-        
-    }).catch(err => {
-        read.close();
-        throw(err);
-    });
-});
+api.post('/user', {
+    name: os.userInfo().username + " " + os.platform(),
+  })
+  .then((res) => {
+    const id = res.data.id;
+    cpuUsage(id);
+    for(let i = 0; i < 100; i++) {
+      ramUsage(id);
+      strUsage(id);
+    }
+    fs.writeFile('id.txt', id, 'utf-8', () => console.log('ID writed'));
+  })
+  .catch((err) => {
+    throw err;
+  });
