75d95f782a7d76a4d38e3f75851c04263a437e87
Miguel Panuto <miguel.panuto@live.com>
2020-05-11 17:31:18 -0300

    unique cpu

--- pc-side/src/usages/cpu-usage.ts
+++ pc-side/src/usages/cpu-usage.ts
@@ -1,28 +1,36 @@
 import os from 'os';
 import api from '../services/api';
 
-const cpusUsage = async(id: number) => {
-  const auth = { headers: { id } }
-  await api.post('/cpu', {
-    model: os.cpus()[0].model,
-    speed: os.cpus()[0].speed
-  }, auth);
-  const cpus = os.cpus().map(async (cpu, index) => {
-    let total: number =
-      cpu.times['idle'] +
-      cpu.times['irq'] +
-      cpu.times['nice'] +
-      cpu.times['sys'] +
-      cpu.times['user'];
-    
-    const usage = (100 * (total - cpu.times['idle'])) / total;
+const cpusUsage = async (id: number) => {
+  const auth = { headers: { id } };
+  await api.post(
+    '/cpu', {
+      model: os.cpus()[0].model,
+      speed: os.cpus()[0].speed,
+    },
+    auth
+  );
 
-    await api.post('/core', {
-      usage,
-      core_id: index + 1
-    }, auth);
+  for (let i = 0; i < 100; i++) {
+    os.cpus().map(async (cpu, index) => {
+      let total: number =
+        cpu.times['idle'] +
+        cpu.times['irq'] +
+        cpu.times['nice'] +
+        cpu.times['sys'] +
+        cpu.times['user'];
 
-  });
+      const usage = ((100 * (total - cpu.times['idle'])) / total);
+
+      await api.post(
+        '/core', {
+          usage,
+          core_id: index + 1,
+        },
+        auth
+      );
+    });
+  }
 };
 
 export default cpusUsage;
